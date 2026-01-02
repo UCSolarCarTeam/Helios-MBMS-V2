45b6a0cf5a97ad750cb359de26e3a09e668301de
millainel <millaine.li@ucalgary.ca>
2026-01-02 11:43:32 -0700

    created startup files

---
+++ MBMS-V2/Core/Inc/StartupTask.h
@@ -0,0 +1,19 @@
+/*
+ * StartupTask.h
+ *
+ *  Created on: Nov 26, 2025
+ *      Author: m
+ */
+
+#ifndef INC_STARTUPTASK_H_
+#define INC_STARTUPTASK_H_
+
+
+#include <stdint.h>
+
+
+void StartupTask(void* arg);
+
+void Startup();
+
+#endif /* INC_STARTUPTASK_H_ */
---
+++ MBMS-V2/Core/Src/StartupTask.c
@@ -0,0 +1,17 @@
+/*
+ * StartupTask.c
+ *
+ *  Created on: Nov 26, 2025
+ *      Author: m
+ */
+
+void StartupTask(void* arg) {
+	while(1) {
+		Startup();
+	}
+}
+
+void Startup(){
+
+}
+
