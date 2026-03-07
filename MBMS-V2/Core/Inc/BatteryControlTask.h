c3d625cd6047272a6483dde7c261a1dd1227401b
millainel <millaine.li@ucalgary.ca>
2026-03-07 11:11:03 -0700

    updated BCT header for all funcs i think

--- MBMS-V2/Core/Inc/BatteryControlTask.h
+++ MBMS-V2/Core/Inc/BatteryControlTask.h
@@ -16,40 +16,59 @@ void BatteryControl();
 void perms_init();
 void MBMSStatus_init();
 
-/* "private" helper functions */
-void initiateBPSFault();
+/* --------- "Private" Helper Functions --------- */
+
 void checkKeyShutdown();
 void updateContactorInfo(uint8_t contactor, uint8_t prechargerClosed, uint8_t prechargerClosing,
 		uint8_t prechargerError, uint8_t contactorClosed, uint8_t contactorClosing,
 		uint8_t contactorError, int16_t lineCurrent, int16_t chargeCurrent, uint8_t BPSerror);
 
+// Private Startup Check Functions
 void startupCheck();
 uint8_t waitForFirstHeartbeats();
 uint8_t startupBatteryCheck();
 uint8_t checkPrechargersOpen();
 uint8_t checkContactorsOpen();
 
+// Private Switch State Functions
+void enter_BOOT();
+
+void enter_MPS_DISCONNECTED();
+
+void enter_BPS_FAULT();
+
+void enter_SOFT_TRIP();
+
+void enter_CHARGING();
+
+void enter_FULLY_OPERATIONAL();
+
+// Private Reset Functions
+
 void clear_Trips();
 void clear_SoftTrips();
 
+// Update BCT Counter Function
+void UpdateCounter(uint32_t * counter);
 
 
-/* "public" functions */
+
+
+/* --------- "Public" Functions --------- */
+
+// Update Struct Functions (including checking trips/strips)
 void Update_ContactorInfoStruct();
 void Update_BatteryInfoStruct();
 void Update_DCDCStackStruct();
-
 void Update_TripStruct();
 void Update_SoftTripStruct();
 
+// Checking CCPS Alive Function
 void Check_ContactorHeartbeats();
 
+// Control Functions
 void SystemStateMachine();
 void Control_Contactors();
-
-void UpdateCounter(uint32_t * counter);
-
-void enter_BOOT();
 
 
 
