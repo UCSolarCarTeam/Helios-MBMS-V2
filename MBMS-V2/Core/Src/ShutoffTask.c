5019b65634ac1576715e7f5cf003dd1f82d53ba8
millainel <millaine.li@ucalgary.ca>
2026-05-09 15:33:50 -0600

    shutdown procedure, might need to check over

---
+++ MBMS-V2/Core/Src/ShutoffTask.c
@@ -0,0 +1,106 @@
+/*
+ * ShutoffTask.c
+ *
+ *  Created on: May 9, 2026
+ *      Author: m
+ */
+
+
+#include <stdint.h>
+
+
+#include "../Inc/ShutoffTask.h"
+#include "StartupTask.h"
+#include "CAN.h"
+#include "cmsis_os.h"
+#include "main.h"
+#include "BatteryControlTask.h"
+#include "ReadGPIO.h"
+#include "MBMS.h"
+
+
+extern Permissions mbmsPermissions;
+extern Contactor_Info contactorInfo[NUM_OF_CNTR];
+
+
+void ShutoffTask(void* arg)
+{
+    while(1)
+    {
+    	Shutoff();
+    }
+}
+
+
+void Shutoff()
+{
+
+	uint32_t flags;
+
+	while (1) {
+
+		// wait for shutoff flag
+		flags = osEventFlagsWait(shutoffFlagHandle, SHUTOFF_FLAG, osFlagsWaitAny | osFlagsNoClear, osWaitForever);
+
+		//disable 12V CAN
+		HAL_GPIO_WritePin(_12V_CAN_EN_GPIO_Port, _12V_CAN_EN_Pin, !_12V_CAN_EN_ACTIVE);
+
+		// ensure charging is disabled
+		HAL_GPIO_WritePin(_14V_Charge_EN_GPIO_Port, _14V_Charge_EN_Pin, !_14V_CHARGE_EN_ACTIVE);
+
+
+		mbmsPermissions.motor = 0;
+		// wait to open CHECK THIS
+		while(contactorInfo[MOTOR].contactor_close == CLOSE_CONTACTOR) {
+
+		}
+
+		mbmsPermissions.array = 0;
+		// wait to open CHECK THIS
+		while(contactorInfo[ARRAY].contactor_close == CLOSE_CONTACTOR) {
+
+		}
+
+		mbmsPermissions.lv = 0;
+		// wait to open CHECK THIS
+		while(contactorInfo[LV].contactor_close == CLOSE_CONTACTOR) {
+
+		}
+
+		mbmsPermissions.charge = 0;
+		// wait to open CHECK THIS
+		while(contactorInfo[CHARGE].contactor_close == CLOSE_CONTACTOR) {
+
+		}
+
+		// Disable DCDC1
+		HAL_GPIO_WritePin(DCDC1_EN_GPIO_Port, DCDC1_EN_Pin, !DCDC1_EN_ACTIVE);
+
+
+		if((flags & HARD_BAT_LIMIT_FLAG) == HARD_BAT_LIMIT_FLAG) {
+			while(1){
+				osDelay(100);
+				// wait for driver to turn off car using key
+			}
+		}
+
+		else {
+			osDelay(200);
+			// start thread for startup!!!
+			if(read_MPS() == MPS_ACTIVE) {
+				enter_BOOT();
+				osEventFlagsDelete(shutoffFlagHandle);
+				shutoffFlagHandle = osEventFlagsNew(&shutoffFlag_attributes);
+				osEventFlagsClear(shutoffFlagHandle, 0xffffffff);
+
+				StartupTaskHandle = osThreadNew(StartupTask, NULL, &StartupTask_attributes);
+			}
+
+		}
+	}
+
+}
+
+
+
+
