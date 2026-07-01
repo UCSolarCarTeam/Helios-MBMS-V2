170140eea0c8f4f46a80288d1d66312c3c29a006
millainel <millaine.li@ucalgary.ca>
2026-07-01 13:27:28 -0600

    lil fixaroo :p

--- MBMS-V2/Core/Src/main.c
+++ MBMS-V2/Core/Src/main.c
@@ -251,7 +251,7 @@ static void MX_FDCAN1_Init(void)
   tempInfoFilter.FilterIndex		= 1;
   tempInfoFilter.FilterType 		= FDCAN_FILTER_MASK;
   tempInfoFilter.FilterConfig	    = FDCAN_FILTER_TO_RXFIFO0;
-  tempInfoFilter.FilterID1		    = MIN_MAX_VOLTAGES_ID;
+  tempInfoFilter.FilterID1		    = TEMP_INFO_ID;
   tempInfoFilter.FilterID2			= 0x1fffffff;
 
   if(HAL_FDCAN_ConfigFilter(&hfdcan1, &tempInfoFilter) != HAL_OK)
@@ -259,18 +259,31 @@ static void MX_FDCAN1_Init(void)
 	  Error_Handler();
   }
 
-  FDCAN_FilterTypeDef maxminVoltagedFilter;
-  maxminVoltagedFilter.IdType 			= FDCAN_EXTENDED_ID; //used in canmsg
-  maxminVoltagedFilter.FilterIndex		= 2;
-  maxminVoltagedFilter.FilterType 		= FDCAN_FILTER_MASK;
-  maxminVoltagedFilter.FilterConfig	    = FDCAN_FILTER_TO_RXFIFO0;
-  maxminVoltagedFilter.FilterID1		= TEMP_INFO_ID;
-  maxminVoltagedFilter.FilterID2	 	= 0x1fffffff;
+  FDCAN_FilterTypeDef cellVoltagesFilter;
+  cellVoltagesFilter.IdType 			= FDCAN_EXTENDED_ID; //used in canmsg
+  cellVoltagesFilter.FilterIndex		= 2;
+  cellVoltagesFilter.FilterType 		= FDCAN_FILTER_MASK;
+  cellVoltagesFilter.FilterConfig	    = FDCAN_FILTER_TO_RXFIFO0;
+  cellVoltagesFilter.FilterID1			= CELL_VOLTAGES_ID;
+  cellVoltagesFilter.FilterID2	 		= 0x1fffffff;
 
-  if(HAL_FDCAN_ConfigFilter(&hfdcan1, &maxminVoltagedFilter) != HAL_OK)
+  if(HAL_FDCAN_ConfigFilter(&hfdcan1, &cellVoltagesFilter) != HAL_OK)
   {
-	  Error_Handler();
+      Error_Handler();
   }
+
+//  FDCAN_FilterTypeDef maxminVoltagedFilter;
+//  maxminVoltagedFilter.IdType 			= FDCAN_EXTENDED_ID; //used in canmsg
+//  maxminVoltagedFilter.FilterIndex		= 2;
+//  maxminVoltagedFilter.FilterType 		= FDCAN_FILTER_MASK;
+//  maxminVoltagedFilter.FilterConfig	    = FDCAN_FILTER_TO_RXFIFO0;
+//  maxminVoltagedFilter.FilterID1		= TEMP_INFO_ID;
+//  maxminVoltagedFilter.FilterID2	 	= 0x1fffffff;
+//
+//  if(HAL_FDCAN_ConfigFilter(&hfdcan1, &maxminVoltagedFilter) != HAL_OK)
+//  {
+//	  Error_Handler();
+//  }
 
   FDCAN_FilterTypeDef contactorFilter;
   contactorFilter.IdType 			= FDCAN_EXTENDED_ID; //used in canmsg
